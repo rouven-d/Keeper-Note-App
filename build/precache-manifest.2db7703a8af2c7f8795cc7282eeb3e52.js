63ea57d14892e3d8c2083b52ef293b8a3dfd29d2
Rouven <rouven.daunke@googlemail.com>
2021-07-08 10:59:12 +0200

    build command run

---
+++ build/precache-manifest.2db7703a8af2c7f8795cc7282eeb3e52.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b3660b3204b28d7f32063f8e6f3d65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d00c614cfed4e450201",
+    "url": "/static/js/2.d2bac8b0.chunk.js"
+  },
+  {
+    "revision": "d71a08f59e59cc369a24",
+    "url": "/static/js/main.0b29c60e.chunk.js"
+  },
+  {
+    "revision": "583e51a7281672351041",
+    "url": "/static/js/runtime-main.38f647cd.js"
+  }
+]);
